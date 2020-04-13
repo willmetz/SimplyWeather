b05185a7323e6fe4d462dccb187a345263c4d255
wmetz <wmetz@ostusa.com>
2020-04-12 21:16:12 -0400

    adding in a bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,44 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:ost_weather/Bloc/LocationBloc.dart';
 import 'package:ost_weather/Bloc/bloc_provider.dart';
 import 'package:ost_weather/UI/HomeScreen.dart';
-//import 'package:ost_weather/UI/MainScreen.dart';
-import 'package:ost_weather/UI/Widgets/CurrentConditionsWidget.dart';
-import 'package:ost_weather/UI/Widgets/HourlyConditionsCell.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(WeatherApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class WeatherApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return WeatherAppState();
+  }
+}
+
+class WeatherAppState extends State<WeatherApp> {
+  BottomNavigationBar _bottomNavigationBar;
+
   @override
   Widget build(BuildContext context) {
+    _bottomNavigationBar = getBottomNavBar();
     LocationBloc bloc = LocationBloc();
 
     return BlocProvider<LocationBloc>(
         bloc: bloc,
         child: MaterialApp(
-          title: 'Flutter Demo',
+          title: 'Simply Weather',
           theme: ThemeData(
             // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
             primarySwatch: Colors.blue,
           ),
           home: FutureBuilder<bool>(
             future: bloc.initLocation(),
             builder: (context, AsyncSnapshot<bool> snapshot) {
-              //return MainScreen();
+              //TODO - set current index based on if location is already set or not
+
               return Scaffold(
+                bottomNavigationBar: _bottomNavigationBar,
                 body: HomeScreen(),
               );
             },
             //child: MainScreen()),
           ),
         ));
+    ;
+  }
+
+  BottomNavigationBar getBottomNavBar() {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.edit_location), title: Text("Location")),
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
+        BottomNavigationBarItem(icon: Icon(Icons.calendar_today), title: Text("Forecast"))
+      ],
+    );
   }
 }
