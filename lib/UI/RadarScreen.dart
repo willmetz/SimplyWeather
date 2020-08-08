0ec03489d748d122d452cce5291d76339c41962c
wmetz <wmetz@ostusa.com>
2020-08-07 21:41:49 -0400

    Location drawing works, need to clean up code and change to a location icon and see if i can make the text larger on the map.  Then add zoom controls.

--- lib/UI/RadarScreen.dart
+++ lib/UI/RadarScreen.dart
@@ -21,7 +21,7 @@ class RadarScreen extends StatelessWidget {
           final RadarData radarData = snapshot.data;
 
           if (radarData == null) {
-            _bloc.getLatestRadar(8);
+            _bloc.getLatestRadar(10);
             return Center(
               child: Text("Loading..."),
             );
@@ -29,35 +29,66 @@ class RadarScreen extends StatelessWidget {
             if (radarData.state == RadarState.noLocationAvailable) {
               return locationRequired();
             }
-            return Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(child: RadarOverlayWidget(radarData.layeredTiles[0])),
-                    Expanded(child: RadarOverlayWidget(radarData.layeredTiles[1])),
-                    Expanded(child: RadarOverlayWidget(radarData.layeredTiles[2])),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(child: RadarOverlayWidget(radarData.layeredTiles[3])),
-                    Expanded(
-                        child: Stack(children: <Widget>[
-                      RadarOverlayWidget(radarData.layeredTiles[4]),
-                      CustomPaint(painter: LocationPainter(radarData.layeredTiles[4].tile.xTileOffsetPercent, radarData.layeredTiles[4].tile.yTileOffsetPercent, Colors.blue))
-                    ])),
-                    Expanded(child: RadarOverlayWidget(radarData.layeredTiles[5])),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(child: RadarOverlayWidget(radarData.layeredTiles[6])),
-                    Expanded(child: RadarOverlayWidget(radarData.layeredTiles[7])),
-                    Expanded(child: RadarOverlayWidget(radarData.layeredTiles[8])),
-                  ],
-                )
+            return Table(
+              border: TableBorder(bottom: BorderSide.none, top: BorderSide.none, left: BorderSide.none, right: BorderSide.none),
+              children: [
+                TableRow(children: [RadarOverlayWidget(radarData.layeredTiles[0]), RadarOverlayWidget(radarData.layeredTiles[1]), RadarOverlayWidget(radarData.layeredTiles[2])]),
+                TableRow(children: [
+                  RadarOverlayWidget(radarData.layeredTiles[3]),
+                  Stack(children: <Widget>[
+                    RadarOverlayWidget(radarData.layeredTiles[4]),
+
+                    LayoutBuilder(
+                      builder: (context, constraints) {
+                        return Container(
+                          height: constraints.maxWidth,
+                          width: constraints.maxWidth,
+                          alignment: Alignment.center,
+                          child: CustomPaint(
+                              size: Size(constraints.maxWidth, constraints.maxWidth),
+                              painter: LocationPainter(radarData.layeredTiles[4].tile.xTileOffsetPercent, radarData.layeredTiles[4].tile.yTileOffsetPercent, Colors.pink)),
+                        );
+                      },
+                    ),
+
+                    //CustomPaint(painter: LocationPainter(radarData.layeredTiles[4].tile.xTileOffsetPercent, radarData.layeredTiles[4].tile.yTileOffsetPercent, Colors.blue))
+                  ]),
+                  RadarOverlayWidget(radarData.layeredTiles[5])
+                ]),
+                TableRow(children: [RadarOverlayWidget(radarData.layeredTiles[6]), RadarOverlayWidget(radarData.layeredTiles[7]), RadarOverlayWidget(radarData.layeredTiles[8])])
               ],
             );
+
+            // return Column(
+            //   children: <Widget>[
+            //     Row(
+            //       mainAxisSize: MainAxisSize.max,
+            //       children: <Widget>[
+            //         RadarOverlayWidget(radarData.layeredTiles[0]),
+            //         RadarOverlayWidget(radarData.layeredTiles[1]),
+            //         RadarOverlayWidget(radarData.layeredTiles[2]),
+            //       ],
+            //     ),
+            //     Row(
+            //       children: <Widget>[
+            //         Expanded(child: RadarOverlayWidget(radarData.layeredTiles[3])),
+            //         Expanded(
+            //             child: Stack(children: <Widget>[
+            //           RadarOverlayWidget(radarData.layeredTiles[4]),
+            //           CustomPaint(painter: LocationPainter(radarData.layeredTiles[4].tile.xTileOffsetPercent, radarData.layeredTiles[4].tile.yTileOffsetPercent, Colors.blue))
+            //         ])),
+            //         Expanded(child: RadarOverlayWidget(radarData.layeredTiles[5])),
+            //       ],
+            //     ),
+            //     Row(
+            //       children: <Widget>[
+            //         Expanded(child: RadarOverlayWidget(radarData.layeredTiles[6])),
+            //         Expanded(child: RadarOverlayWidget(radarData.layeredTiles[7])),
+            //         Expanded(child: RadarOverlayWidget(radarData.layeredTiles[8])),
+            //       ],
+            //     )
+            //   ],
+            // );
           }
         },
       )),
